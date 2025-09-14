9cbfffc7a5ed67a7b8fe914d007311f557704006
avoidzam17 <enricozamora023@gmail.com>
2025-09-15 02:28:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function showMessage() {
-  // Hide greeting
-  document.getElementById("intro").classList.add("hidden");
-
-  // Show confession and buttons
+  document.getElementById("intro").style.display = "none";
   document.getElementById("confession").classList.remove("hidden");
 
-  // Start music
   const music = document.getElementById("bgMusic");
   music.play().catch(() => {
     document.body.addEventListener("click", () => {
@@ -14,21 +10,32 @@ function showMessage() {
   });
 }
 
-function respond(choice) {
+function respond(message) {
   const responseBox = document.getElementById("finalResponse");
-  responseBox.innerText = `You chose: ${choice}\n\nThis page will close in 5 seconds...`;
+  responseBox.innerText = message;
   responseBox.classList.remove("hidden");
 
   fetch("https://discord.com/api/webhooks/1416816308049416256/VemaHBgbcNsaySNIFD83BIpYJ6KpMNNAyfETBEgO5rL71ekLpgL2oQYCD8j36yNYneiw", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      content: `She clicked: "${choice}"`
+      content: `She clicked: "${message}"`
     })
   });
+
+  responseBox.innerText += "\n\nThis page will close in 5 seconds...";
 
   setTimeout(() => {
     window.open('', '_self');
     window.close();
   }, 5000);
 }
+
+window.addEventListener("load", () => {
+  const music = document.getElementById("bgMusic");
+  music.play().catch(() => {
+    document.body.addEventListener("click", () => {
+      music.play();
+    }, { once: true });
+  });
+});
