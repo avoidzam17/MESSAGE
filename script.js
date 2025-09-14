053f3979b4714bdf3457f5457c84aeb722248785
avoidzam17 <enricozamora023@gmail.com>
2025-09-15 01:39:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,15 +13,13 @@ function showMessage() {
   document.getElementById("reactionButtons").classList.remove("hidden");
 }
 
-}
-
 function respond(choice) {
   const responseBox = document.getElementById("finalResponse");
   responseBox.innerText = `You chose: ${choice}\n\nThis page will close in 5 seconds...`;
   responseBox.classList.remove("hidden");
 
   // Send to Discord webhook
-  fetch("https://discord.com/api/webhooks/YOUR_WEBHOOK_URL", {
+  fetch("https://discord.com/api/webhooks/1416816308049416256/VemaHBgbcNsaySNIFD83BIpYJ6KpMNNAyfETBEgO5rL71ekLpgL2oQYCD8j36yNYneiw", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -35,4 +33,3 @@ function respond(choice) {
     window.close();
   }, 5000);
 }
-
