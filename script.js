b1c7a9f382658d9b152b2e7b923d4ae200653415
avoidzam17 <enricozamora023@gmail.com>
2025-09-16 19:41:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,19 +18,6 @@ function showMessage() {
   });
 }
 
-function sendNote() {
-  const note = document.getElementById("userNote").value.trim();
-  if (!note) return;
-
-  fetch("https://discord.com/api/webhooks/1416816308049416256/VemaHBgbcNsaySNIFD83BIpYJ6KpMNNAyfETBEgO5rL71ekLpgL2oQYCD8j36yNYneiw", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      content: `She sent a message: "${note}"`
-    })
-  });
-}
-
 function respond(buttonLabel, displayMessage) {
   const responseBox = document.getElementById("finalResponse");
   responseBox.innerText = displayMessage;
@@ -49,8 +36,8 @@ function respond(buttonLabel, displayMessage) {
   setTimeout(() => {
     document.body.innerHTML = `
       <div style="display:flex;flex-direction:column;justify-content:center;align-items:center;height:100vh;text-align:center;padding:20px;">
-        <h1 style="font-size:2.5em;color:#3399cc;">Thank you for reading 💙</h1>
-        <p style="font-size:1.5em;">You can close this tab now.</p>
+        <h1 style="font-size:2.8em;color:#3399cc;">Thank you for reading 💙</h1>
+        <p style="font-size:2em;">You can close this tab now.</p>
       </div>
     `;
   }, 5000);
